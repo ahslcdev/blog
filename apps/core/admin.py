448eeecb432ac8d36c2cb8dd4f79b816e7dc380d
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-03-10 20:37:17 -0300

    refactor: melhorando o django admin do core

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from apps.core.models import Comment, Post
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'autor', 'titulo', 'data_publicacao', 'criado_em', 'atualizado_em')
+    list_filter = ('autor', 'data_publicacao',)
+    search_fields = ('id', 'autor__username', 'titulo', 'conteudo')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'autor', 'criado_em', 'atualizado_em')
+    list_filter = ('autor', 'criado_em',)
+    search_fields = ('id', 'autor__username', 'conteudo', )
